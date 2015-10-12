51a47ad99801c7750a3ff5e047c7f5bc61e6e259
Truc Viet Le <vietexob@gmail.com>
2015-10-12 21:26:51 +0800

    Implementing A star

--- code/advanced/flood_fill.py
+++ code/advanced/flood_fill.py
@@ -3,3 +3,84 @@ Created on Oct 11, 2015
 
 @author: trucvietle
 '''
+
+import numpy as np
+from linecache import getline
+
+def floodFill(c, r, mask):
+    """
+    Crawls a mask array containing only 1 and 0 values from the
+    starting point (c = column, r = row) and returns an array with
+    all values connected to the starting cell. This algorithm performs
+    a 4-way check non-recursively.
+    """
+    ## Cells already filled
+    filled = set()
+    ## Cells to fill
+    fill = set()
+    fill.add((c, r))
+    width = mask.shape[1]-1
+    height = mask.shape[0]-1
+    ## Out output inundation array
+    flood = np.zeros_like(mask, dtype=np.int8)
+    ## Loop through the modify the cells that need to be checked
+    while fill:
+        ## Grab a cell
+        x, y = fill.pop()
+        if y == height or x == width or x < 0 or y < 0:
+            ## Dont't fill
+            continue
+        if mask[y][x] == 1:
+            ## Do fill
+            flood[y][x] = 1
+            filled.add((x, y))
+            ## Check neighbors for 1 values
+            west = (x-1, y)
+            east = (x+1, y)
+            north = (x, y-1)
+            south = (x, y+1)
+            if west not in filled:
+                fill.add(east)
+            if east not in filled:
+                fill.add(east)
+            if north not in filled:
+                fill.add(north)
+            if south not in filled:
+                fill.add(south)
+    return flood
+
+source = './data/advanced/FloodFill/terrain.asc'
+target = './data/advanced/FloodFill/flood.asc'
+
+print('Opening image...')
+img = np.loadtxt(source, skiprows=6)
+print('Image opened')
+
+a = np.where(img < 70, 1, 0)
+print('Image masked')
+
+## Parse the header using a loop and the builtin linecache module
+hdr = [getline(source, i) for i in range(1, 7)]
+values = [float(h.split(' ')[-1].strip()) for h in hdr]
+cols, rows, lx, ly, cell, nd = values
+xres = cell
+yres = cell * -1
+
+## Starting point for the flood inundation
+sx = 2582
+sy = 2057
+
+print('Beginning flood fill')
+flood = floodFill(sx, sy, a)
+print('Finnished flood fill')
+
+header = ''
+for i in range(6):
+    header += hdr[i]
+
+print('Saving grid')
+## Open the output file, add the hdr and save the array
+with open(target, 'wb') as f:
+    f.write(bytes(header))
+    np.savetxt(f, flood, fmt='%li')
+print('Done!')
--- code/advanced/least_cost_path.py
+++ code/advanced/least_cost_path.py
@@ -3,3 +3,87 @@ Created on Oct 11, 2015
 
 @author: trucvietle
 '''
+
+import numpy as np
+
+## Width and height of grid
+w = 5
+h = 5
+
+## Start location: lower left of grid
+start = (h-1, 0)
+## End location: top right of grid
+dx = w-1
+dy = 0
+
+## Blank grid
+a = np.zeros((w, h))
+
+## Distance grid
+dist = np.zeros(a.shape, dtype=np.int8)
+## Calculate distance for all cells
+for y, x in np.ndindex(a.shape):
+    dist[y][x] = abs((dx-x)+(dy-y))
+
+## Terrain is a random value between 1-16
+## Add to the distance grid to calculate the cost of moving to a cell
+cost = np.random.randint(1, 16, (w, h)) + dist
+
+print("Cost Grid (Value + Distance)")
+print(cost)
+
+## Our A* search algorithm
+def astar(start, end, h, g):
+    cset = set()
+    oset = set()
+    path = set()
+    oset.add(start)
+    while oset:
+        cur = oset.pop()
+        if cur == end:
+            return path
+        cset.add(cur)
+        path.add(cur)
+        options = []
+        y1 = cur[0]
+        x1 = cur[1]
+        if y1 > 0:
+            options.append((y1-1, x1))
+        if y1 < h.shape[0]-1:
+            options.append((y1+1, x1))
+        if x1 > 0:
+            options.append((y1, x1-1))
+        if x1 < h.shape[1]-1:
+            options.append((y1, x1+1))
+        if end in options:
+            return path
+        best = options[0]
+        cset.add(options[0])
+        for i in range(1, len(options)):
+            option = options[i]
+            if option in cset:
+                continue
+            elif h[option] <= h[best]:
+                best = option
+                cset.add(option)
+            elif g[option] < g[best]:
+                best = option
+                cset.add(option)
+            else:
+                cset.add(option)
+        print(best, ', ', h[best], ', ', g[best])
+        oset.add(best)
+    return []
+
+print ('(Y, X), Heuristic, Distance')
+## Find the path
+path = astar(start, (dy, dx), cost, dist)
+
+## Create and populate the path grid
+path_grid = np.zeros(cost.shape, dtype = np.uint8)
+for y, x in path:
+    path_grid[y][x] = 1
+path_grid[dy][dx] = 1
+
+print('Path Grid: 1=path')
+print(path_grid)
